2cc2ba32dee0e4ff41e7280cdade7eb63554b79f
NazarStf <hworker98@gmail.com>
2023-08-14 18:46:18 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by NazarStf on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
